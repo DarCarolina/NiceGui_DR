3db30480206cd43f06f12f056a911711cc6353b5
DarCarolina <dona@gbmpaulovi.edu.ec>
2026-01-09 14:58:19 -0500

    nuevoo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from nicegui import ui
 #Funciones
 
 #Estructura de la interfaz de usuario
-with ui.card():
-    ui.label('Hola, Mundo!')
+with ui.card().classes('w-full max-w-md mx-auto mt-10 p-6'):
+    ui.label('Hola, Mundo!').classes('fontserif')
 
 ui.run()
